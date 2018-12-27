769daf5c6c17839dc7eaa3d5a1f9dadcbd49d4e6
Minh Vu Pham <45495664+S4ltedF1sh@users.noreply.github.com>
2018-12-27 22:24:17 +0100

    Update Sort.c

--- test/Sort.c
+++ test/Sort.c
@@ -7,10 +7,7 @@
 JNIEXPORT void JNICALL Java_Sort_sort (JNIEnv *env, jclass obj, jintArray arr){
 	int len = (int)(*env)->GetArrayLength(env, arr);
 	jint *a = (*env)->GetIntArrayElements(env, arr, 0);
-	//jintArray ja = (*env)->NewIntArray(env, len);
 	sort((int*)a, len);
-	//(*env)->SetIntArrayRegion(env, ja, 0, len, a);
 	(*env)->SetIntArrayRegion(env, arr, 0, len, a);
-	//return ja;
 }
 
